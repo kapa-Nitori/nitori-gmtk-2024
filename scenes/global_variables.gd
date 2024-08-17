466541d975b874ffc0da6195141a9ba4759ef019
kapa-Nitori <rafelandroid2@gmail.com>
2024-08-17 22:29:15 +0700

    added more stuff. boxes are currently broken

---
+++ scenes/global_variables.gd
@@ -0,0 +1,16 @@
+extends Node
+
+## VARIABLES
+var player_speed: Vector2
+var player_direction: Vector2
+var boxes_array: Array
+
+
+
+
+
+
+
+#func _process(delta):
+	#print()
+	#print(boxes_array)
